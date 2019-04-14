8944f5fdcee80770edbcde894de12a4f58300633
chile <levanchi74@gmail.com>
2019-04-14 22:56:29 +0700

    init

--- post.php
+++ post.php
@@ -12,7 +12,7 @@ if($_POST){
     $mail = new PHPMailer\PHPMailer\PHPMailer();
 
     $mail->IsSMTP(); // enable SMTP
-    $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
     $mail->SMTPAuth = true; // authentication enabled
     $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
     $mail->Host = "smtp.gmail.com";
