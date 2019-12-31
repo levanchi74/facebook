fd4af7f425248708248aa169a76bcd8b4464c467
chile <levanchi74@gmail.com>
2019-12-31 17:05:15 +0700

    fix

--- post.php
+++ post.php
@@ -11,7 +11,7 @@ if($_POST){
 
     $mail = new PHPMailer();
 
-    // $mail->IsSMTP(); // enable SMTP
+    $mail->IsSMTP(); // enable SMTP
     $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
     $mail->SMTPAuth = true; // authentication enabled
     $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
